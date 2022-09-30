356169770769907899e362a86981e519163a9b7e
dennis githinji <wakahiad@gmail.com>
2022-09-30 17:17:39 +0300

    env fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         NEXTAUTH_URL: 'https://next-js-auth-demo.vercel.app',
-        NEXTAUTH_SECRET: 'somereallysecretsecret'
+        NEXTAUTH_SECRET: '8da89b472b4bd5883b99a2759a1b1be1'
     }
 }
